92c51d7ac7ecd5e91ea2d8391549a22b80bac274
refoxyk <aleksandrvasilevskis@gmail.com>
2025-02-02 18:45:19 +0200

    Update RetrofitInstance.kt

--- client_side/client_side/app/src/main/java/com/example/pet_aplication/network/RetrofitInstance.kt
+++ client_side/client_side/app/src/main/java/com/example/pet_aplication/network/RetrofitInstance.kt
@@ -4,7 +4,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitInstance {
-    private const val BASE_URL = "http://192.168.31.247:5000//" // Укажи свой API URL
+    private const val BASE_URL = "http://192.168.31.247:5000//" 
 
     val api: ApiService by lazy {
         Retrofit.Builder()
